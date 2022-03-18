6dc76aa319be003e216f0d754e07abc27d6b2643
leeegarden <j_one_8@naver.com>
2022-03-19 02:06:35 +0900

    css추가

---
+++ assets/js/login.js
@@ -0,0 +1,8 @@
+const navLeft = document.querySelector('.nav-left');
+const navBtn = document.querySelector('.navbtn');
+
+navLeft.addEventListener('mouseover',function(e) {
+
+    navBtn.style.color = #fff;
+});
+
